4ea9b340ed2d128eac3bb2e0eefb05d822dc945c
Feliciano  MANDJAM <felicianomandja@gmail.com>
2024-05-24 16:07:16 +0200

    Sprint 1 Ticket 2 Features : Gestion des pages , ajouts de NavBar, Bootstrap , FontAwesame , Layout et ces routes

--- FRONT/Front/src/App.tsx
+++ FRONT/Front/src/App.tsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
+
+// Pour la gestion de mes routes
+// La dependance que me permet mettre en place mes routes
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+// J'importe les pages 
+import Home from './pages/home/home';
+import Contact from './pages/contact/contact';
+
+// style
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
-function App() {
- 
+// Layout 
+import Layout from './componants/layout'
 
+function App() {
+  
   return (
-    <>
-     <h1> Page Index</h1>
-    </>
+    <BrowserRouter>
+        <Layout>
+      <Routes>
+        <Route index path="/" element={<Home />}/>
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
+        </Layout>
+    </BrowserRouter>
   )
 }
 
 export default App
+
+
+// Une page d'accueil attractive.
+// Une page produit pour présenter ton e-book.
+// Un système de panier et de paiement sécurisé.
+// Un espace utilisateur pour gérer les comptes et les achats.
---
+++ FRONT/Front/src/componants/header.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom';
+
+// Composant assets du site react-bootstrap
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown'; 
+
+const Header = () => {
+  return (
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default Header
