f0c2fb0080e282c870cc0599294a79fc9d3665d6
Feliciano  MANDJAM <felicianomandja@gmail.com>
2024-07-05 12:15:45 +0200

    Test validé

--- BACK/Test/user.test.js
+++ BACK/Test/user.test.js
@@ -1,29 +1,48 @@
-import express from 'express'
-import request from 'supertest'
-import userRoute from '../routes/user.router.js'
+import express from "express";
+import request from "supertest";
+import userRoute from "../routes/user.router.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/user',userRoute)
+app.use("/user", userRoute);
 
+describe("should test all request from API /user/", () => {
+  // SI j'arriver a récupérer les users de la BDD
 
-describe('should test all request from API /user/', () => {
-    it('should test GET from the API /user/all', async () => {
-       const {body , statusCode} = await request(app).get("/user/all") 
-       expect(body).toEqual(
-        expect.arrayContaining([
-            expect.objectContaining({
-                id: expect.any(Number),
-                firstName: expect.any(String),
-                lastName: expect.any(String),
-                email: expect.any(String),
-                password: expect.any(String),
-            })
-        ])
-       )
-    });
-    
+  it("should test GET from the API /user/all", async () => {
+    const { body, statusCode } = await request(app).get("/user/all");
+    expect(body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          firstName: expect.any(String),
+          lastName: expect.any(String),
+          email: expect.any(String),
+          password: expect.any(String),
+        }),
+      ])
+    );
+    expect(statusCode).toBe(200);
+  });
+
+  // Gestion du catch erreur 500 si jamais j'arrive pas a trouver dans la base de donnée
+
+  //   it("should return a error 500", async () => {
+  //     const { body, statusCode } = await request(app).get("/user/all");
+  //     expect(body).toEqual(
+  //       expect.arrayContaining([
+  //         expect.objectContaining({
+  //           id: expect.any(Number),
+  //           firstName: expect.any(String),
+  //           lastName: expect.any(String),
+  //           email: expect.any(String),
+  //           password: expect.any(String),
+  //         }),
+  //       ])
+  //     );
+  //     expect(statusCode).toBe(500);
+  //   });
+  
 });
-
